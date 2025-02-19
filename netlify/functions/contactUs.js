fb3a0d40d7dd9b4550ffbae29a7f47169f303f68
Jon <jonathanlam09@gmail.com>
2025-02-19 20:54:43 +0800

    Branch name: master Description: - remove testing code

--- netlify/functions/contactUs.js
+++ netlify/functions/contactUs.js
@@ -23,7 +23,6 @@ exports.handler = async (event, context) => {
         }
 
         const body = JSON.parse(event.body);
-        throw new Error(event.body)
         const validator_res = await checkSchema({
             fullname: {
                 notEmpty: {
